29a3a506cc292610a9d15306a37a9143d1bdc7ca
me-coderaider <kashyappankajnsp@gmail.com>
2025-06-13 18:10:33 +0530

    Journal class following the SRP

---
+++ src/designpatterns/solid/s/SRP.java
@@ -0,0 +1,32 @@
+package designpatterns.solid.s;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class Journal {
+	private final List<String> entries = new ArrayList<>();
+
+	private static int count = 0;
+
+	public void addEntry(String text) {
+		entries.add("" + (++count) + ": " + text);
+	}
+
+	public void removeEntry(int index) {
+		entries.remove(index);
+	}
+
+	@Override
+	public String toString() {
+		return String.join(System.lineSeparator(), entries);
+	}
+}
+
+class SRP {
+	public static void main(String[] args) throws Exception {
+		Journal j = new Journal();
+		j.addEntry("I cried today");
+		j.addEntry("I ate a bug");
+		System.out.println(j);
+	}
+}
