6ec174589b293ff17528ce6e38fc9044cbc951cb
me-coderaider <kashyappankajnsp@gmail.com>
2025-06-17 16:01:16 +0530

    Simple Rectangle class to calculate its area.

---
+++ src/designpatterns/solid/l/LSP.java
@@ -0,0 +1,47 @@
+package designpatterns.solid.l;
+
+class Rectangle {
+	protected int width, height;
+
+	public Rectangle() {
+	}
+
+	public Rectangle(int width, int height) {
+		this.width = width;
+		this.height = height;
+	}
+
+	public int getWidth() {
+		return width;
+	}
+
+	public void setWidth(int width) {
+		this.width = width;
+	}
+
+	public int getHeight() {
+		return height;
+	}
+
+	public void setHeight(int height) {
+		this.height = height;
+	}
+
+	public int getArea() {
+		return width * height;
+	}
+
+	@Override
+	public String toString() {
+		return "Rectangle{" + "width=" + width + ", height=" + height + '}';
+	}
+}
+
+class LSP {
+
+	public static void main(String[] args) {
+		Rectangle rc = new Rectangle(2, 3);
+		
+		System.out.println(rc.getArea());
+	}
+}
