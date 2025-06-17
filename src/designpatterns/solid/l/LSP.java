4524b774e5ca23820f1cbbca24adec404421755e
me-coderaider <kashyappankajnsp@gmail.com>
2025-06-17 17:55:54 +0530

    Breaking LSP and honoring LSP using a factory pattern or by a method

--- src/designpatterns/solid/l/LSP.java
+++ src/designpatterns/solid/l/LSP.java
@@ -35,13 +35,58 @@ class Rectangle {
 	public String toString() {
 		return "Rectangle{" + "width=" + width + ", height=" + height + '}';
 	}
+
+	public boolean isSquare() { // detecting whether the passed parameters form a 'rectangle' or 'square' 
+		return width == height;
+	}
+}
+
+class Square extends Rectangle {
+	public Square() {
+	}
+
+	public Square(int size) {
+		width = height = size;
+	}
+
+	@Override // adding special overrides to set width & height
+	public void setWidth(int width) {
+		super.setWidth(width);
+		super.setHeight(width);
+	}
+
+	@Override
+	public void setHeight(int height) {
+		super.setHeight(height);
+		super.setWidth(height);
+	}
+}
+
+class RectangleFactory { // using 'factory' pattern to explicitly create 'square' & 'rectangle' objects. 
+	public static Rectangle newSquare(int side) {
+		return new Rectangle(side, side);
+	}
+
+	public static Rectangle newRectangle(int width, int height) {
+		return new Rectangle(width, height);
+	}
 }
 
 class LSP {
+	// maybe conform to ++
+	static void useIt(Rectangle r) {
+		int width = r.getWidth();
+		r.setHeight(10); // bad kind of setter when we store 'Square' objects in Rectangle 
+		System.out.println("Expected area of " + (width * 10) + ", got " + r.getArea());
+	}
 
 	public static void main(String[] args) {
 		Rectangle rc = new Rectangle(2, 3);
-		
-		System.out.println(rc.getArea());
+		useIt(rc);
+
+		Rectangle sq = new Square();
+//		sq.setHeight(5);
+		sq.setWidth(5); // getting wrong answer, expected area: 25, but got 100
+		useIt(sq);
 	}
 }
