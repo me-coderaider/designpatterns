ec8261bf95314cab7783374d15750f38f86402e7
me-coderaider <kashyappankajnsp@gmail.com>
2025-06-25 15:22:24 +0530

    Resolving Point creation issue with Enum by making a COMPLEX constructor

--- src/designpatterns/creational/factory/FactoryDemo.java
+++ src/designpatterns/creational/factory/FactoryDemo.java
@@ -1,21 +1,28 @@
 package designpatterns.creational.factory;
 
+enum CoordinateSystem {
+	CARTESIAN, POLAR
+}
+
 class Point {
 	private double x, y;
 
-	public Point(double x, double y) {
-		this.x = x;
-		this.y = y;
-	}
-	
-	public Point(double rho, double theta) { // Illegal & not acceptable to have constructor with same signature.
-		this.x = rho * Math.cos(theta);
-		this.y = rho * Math.sin(theta);
+	public Point(double a, double b, CoordinateSystem cs) {
+		switch (cs) {
+		case CARTESIAN:
+			this.x = a;
+			this.y = b;
+			break;
+		case POLAR:
+			this.x = a * Math.cos(b);
+			this.y = a * Math.sin(b);
+			break;
+		}
 	}
 }
 
 class FactoryDemo {
 	public static void main(String[] args) {
-		
+	    Point point = new Point(2, 3, CoordinateSystem.CARTESIAN);
 	}
 }
