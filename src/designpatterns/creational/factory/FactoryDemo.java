c6e0c4d0d684f2522f18ed555369367ac55b070a
me-coderaider <kashyappankajnsp@gmail.com>
2025-06-25 15:44:53 +0530

    Resolving 'Factory' issue by moving it inside the main class
    i.e. a nested factory and keep the constructor private

--- src/designpatterns/creational/factory/FactoryDemo.java
+++ src/designpatterns/creational/factory/FactoryDemo.java
@@ -8,22 +8,24 @@ class Point {
 		this.y = y;
 
 	}
+	
+	public static class Factory {
+	//public static class PointFactory { // no need to call it 'PointFactory' we can call it just Factory
+		// Factory Method
+		public static Point newCartesianPoint(double x, double y) {
+			return new Point(x, y);
+		}
+		
+		public static Point newPolarPoint(double rho, double theta) {
+			return new Point(rho * Math.cos(theta), rho * Math.sin(theta));
+		}
+	}
 }
 
-class PointFactory {
-	// Factory Method
-	public static Point newCartesianPoint(double x, double y) {
-		return new Point(x, y);
-	}
-
-	public static Point newPolarPoint(double rho, double theta) {
-		return new Point(rho * Math.cos(theta), rho * Math.sin(theta));
-	}
-}
 
 class FactoryDemo {
 	public static void main(String[] args) {
-		Point point = PointFactory.newPolarPoint(2, 3);
-		Point point1 = new Point(4, 5);
+		Point point = Point.Factory.newPolarPoint(2, 3);
+//		Point point1 = new Point(4, 5); // can't use as constructor is private
 	}
 }
