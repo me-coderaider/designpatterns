b3ba6aa9e3a4d45a528eda3cef92defdb50a4f80
me-coderaider <kashyappankajnsp@gmail.com>
2025-06-25 13:36:09 +0530

    Fixing multiple builders' inheritance issue with 'RECURSIVE GENERICS'

--- src/designpatterns/creational/builder/FluentBuilderWithRecursiveGenerics.java
+++ src/designpatterns/creational/builder/FluentBuilderWithRecursiveGenerics.java
@@ -10,34 +10,42 @@ class Person {
 	}
 }
 
-class PersonBuilder {
+class PersonBuilder<SELF extends PersonBuilder<SELF>> {
 	protected Person person = new Person();
 
-	public PersonBuilder withName(String name) {
+	public SELF withName(String name) {
 		person.name = name;
-		return this;
+		return self();
 	}
 
 	public Person build() {
 		return person;
 	}
+
+	protected SELF self() {
+		return (SELF) this;
+	}
 }
 
-class EmployeeBuilder extends PersonBuilder{
+class EmployeeBuilder extends PersonBuilder<EmployeeBuilder> {
 	public EmployeeBuilder worksAt(String position) {
-		person.position=position;
+		person.position = position;
+		return self();
+	}
+
+	@Override
+	protected EmployeeBuilder self() {
 		return this;
 	}
+
 }
 
 public class FluentBuilderWithRecursiveGenerics {
 	public static void main(String[] args) {
 
 		EmployeeBuilder pb = new EmployeeBuilder();
-		Person person = pb
-					.withName("John")
-					.worksAt() // function is not available
-					.build();
-		System.out.println(person);
+		Person john = pb.withName("John").worksAt("Continental") // function is not available
+				.build();
+		System.out.println(john);
 	}
 }
