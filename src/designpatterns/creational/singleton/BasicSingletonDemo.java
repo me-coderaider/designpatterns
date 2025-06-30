436868b089b46855b1c48c5896f7cd056c064aed
me-coderaider <kashyappankajnsp@gmail.com>
2025-06-30 15:10:05 +0530

    1. Serialization - New object creation due to de-serialization

--- src/designpatterns/creational/singleton/BasicSingletonDemo.java
+++ src/designpatterns/creational/singleton/BasicSingletonDemo.java
@@ -1,6 +1,12 @@
 package designpatterns.creational.singleton;
 
-class BasicSingleton {
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
+
+class BasicSingleton implements Serializable {
 	// cannot new this class, however
 	// * instance can be created deliberately (reflection)
 	// * instance can be created accidentally (serialization)
@@ -27,10 +33,35 @@ class BasicSingleton {
 
 class BasicSingletonDemo {
 
-	public static void main(String[] args) {
+	static void saveToFile(BasicSingleton singleton, String filename) throws Exception {
+		try (FileOutputStream fileOut = new FileOutputStream(filename);
+				ObjectOutputStream out = new ObjectOutputStream(fileOut)) {
+			out.writeObject(singleton);
+		}
+	}
+
+	static BasicSingleton readFromFile(String filename) throws Exception {
+		try (FileInputStream fileIn = new FileInputStream(filename);
+				ObjectInputStream in = new ObjectInputStream(fileIn)) {
+			return (BasicSingleton) in.readObject();
+		}
+	}
+
+	public static void main(String[] args) throws Exception {
 		BasicSingleton singleton = BasicSingleton.getInstance();
-		singleton.setValue(121);
+		singleton.setValue(111);
+
+		String filename = "singleton.bin";
+		saveToFile(singleton, filename);
+
+		singleton.setValue(222);
+
+		BasicSingleton singleton2 = readFromFile(filename);
+
+		System.out.println(singleton == singleton2);
 
 		System.out.println(singleton.getValue());
+		System.out.println(singleton2.getValue());
+
 	}
 }
