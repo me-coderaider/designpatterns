75cecc398146dab491f8c33cfcf88c8d427100c2
me-coderaider <kashyappankajnsp@gmail.com>
2025-06-30 16:19:50 +0530

    Thread safe singleton using inner-static class

---
+++ src/designpatterns/creational/singleton/InnerStaticSingletonDemo.java
@@ -0,0 +1,20 @@
+package designpatterns.creational.singleton;
+
+class InnerStaticSingleton {
+
+	private InnerStaticSingleton() {
+	}
+
+	private static class Impl {
+		private static final InnerStaticSingleton INSTANCE = new InnerStaticSingleton();
+	}
+
+	public InnerStaticSingleton getInstance() {
+		return Impl.INSTANCE;
+	}
+
+}
+
+public class InnerStaticSingletonDemo {
+
+}
