1d3c4781eb0be67814e2d4a1fea16b32d44badcf
me-coderaider <kashyappankajnsp@gmail.com>
2025-06-30 14:26:38 +0530

    Copying objects and their hierarchies using 'Serialization' easily.

---
+++ src/designpatterns/creational/prototype/CopyThroughSerializationDemo.java
@@ -0,0 +1,35 @@
+package designpatterns.creational.prototype;
+
+import org.apache.commons.lang3.SerializationUtils;
+
+import java.io.Serializable;
+
+// some libraries use reflection (no need for Serializable)
+class Foo implements Serializable {
+	public int stuff;
+	public String whatever;
+
+	public Foo(int stuff, String whatever) {
+		this.stuff = stuff;
+		this.whatever = whatever;
+	}
+
+	@Override
+	public String toString() {
+		return "Foo{" + "stuff=" + stuff + ", whatever='" + whatever + '\'' + '}';
+	}
+}
+
+class CopyThroughSerializationDemo {
+	public static void main(String[] args) {
+		Foo foo = new Foo(42, "life");
+		// use apache commons!
+		Foo foo2 = SerializationUtils.roundtrip(foo); // roundtrip() method does 2 things, it serializes & deserializes
+														// the object.
+
+		foo2.whatever = "xyz";
+
+		System.out.println(foo);
+		System.out.println(foo2);
+	}
+}
